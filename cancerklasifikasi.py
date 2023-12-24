e2bf2e321c87bd29093c3261689ee7c5a8a7bf91
Muhammad Irpan <120390988+Smillerby@users.noreply.github.com>
2023-12-24 20:53:14 +0700

    Update cancerklasifikasi.py

--- cancerklasifikasi.py
+++ cancerklasifikasi.py
@@ -13,13 +13,12 @@ st.title('Prediksi Keparahan Kanker Paru-Paru')
 
 # Tambahkan input pengguna menggunakan widget streamlit
 age = st.slider('Usia Pasien', min_value=1, max_value=100, value=50)
-air_pollution = st.slider('Tingkat Polusi Udara', min_value=1, max_value=10, value=5)
 # Tambahkan input lain sesuai kebutuhan
 
 # Tombol untuk melakukan prediksi
 if st.button('Prediksi'):
     # Lakukan prediksi menggunakan fungsi predict_severity
-    input_data = (0, age, air_pollution, 3, 1, 5, 3, 4, 2, 2, 2, 2, 4, 2, 3)  # Sesuaikan dengan input pengguna
+    input_data = (1, age, 1, 3, 1, 5, 3, 4, 2, 2, 2, 2, 4, 2, 3)  # Sesuaikan dengan input pengguna
     hasil_prediksi = predict_severity(input_data)
 
     # Tampilkan hasil prediksi
