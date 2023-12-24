a7caf5aba8c32b7932f1fd634da77599dddfb8cd
Muhammad Irpan <120390988+Smillerby@users.noreply.github.com>
2023-12-24 22:30:56 +0700

    Update cancerklasifikasi.py

--- cancerklasifikasi.py
+++ cancerklasifikasi.py
@@ -2,7 +2,7 @@ import pickle
 import numpy as np
 import streamlit as st
 
-model = pickle.load(open('cancer patient data sets.sav', 'rb'))
+model = pickle.load(open('LungCancer.sav', 'rb'))
 
 st.title('Prediksi Penyakit Kanker Paru-Paru')
 c1, c2, c3 = st.columns(3)
@@ -18,7 +18,6 @@ with c2:
     Age = st.number_input('Umur Pasien')
     Alcohol_use = st.number_input('Alkohol yang di konsumsi')
     Genetic_Risk = st.number_input('Resiko Genetik')
-    Chest_Pain = st.number_input('Nyeri Dada')
     Weight_Loss  = st.number_input('Penurunan Berat Badan')
     Swallowing_Difficulty = st.number_input('Kesulitan Menelan')
 
@@ -26,13 +25,13 @@ with c3:
     Gender = st.number_input('Jenis Kelamin Pasien')
     Dust_Allergy = st.number_input('Alergi Debu')
     Smoking = st.number_input('Riwayat Paien Merokok')
+    Coughing_of_Blood = st.number_input('Batuk Berdarah')
     Shortness_of_Breath = st.number_input('Sesak Nafas')
-    
 
 prediksi = ''
 if st.button('Hasil Prediksi'):
     prediksi = model.predict([[ Age,	Gender,	Air_Pollution, Alcohol_use, Dust_Allergy, OccuPational_Hazards,	Genetic_Risk, 
-                                Balanced_Diet, Smoking, Fatigue, 
+                                Balanced_Diet, Smoking, Coughing_of_Blood, Fatigue, 
                                Weight_Loss, Shortness_of_Breath, Wheezing, Swallowing_Difficulty]])
 
     if (prediksi [0] == 0):
