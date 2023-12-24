edc050ed7c32e17eb1cb4dfd21c077856a7a1bde
Muhammad Irpan <120390988+Smillerby@users.noreply.github.com>
2023-12-24 22:35:57 +0700

    Update cancerklasifikasi.py

--- cancerklasifikasi.py
+++ cancerklasifikasi.py
@@ -1,43 +1,30 @@
+import streamlit as st
 import pickle
 import numpy as np
-import streamlit as st
 
-model = pickle.load(open('cancer patient data sets.sav', 'rb'))
+# Load the saved model
+filename = 'cancer patient data sets.sav'
+model = pickle.load(open(filename, 'rb'))
 
-st.title('Prediksi Penyakit Kanker Paru-Paru')
-c1, c2, c3 = st.columns(3)
+# Streamlit UI
+st.title('Keparahan Kanker Paru-Paru Predictor')
 
-with c1:
-    Air_Pollution = st.number_input('Polusi Udara')
-    OccuPational_Hazards = st.number_input('Bahaya Pekerjaan')
-    Balanced_Diet = st.number_input('Diet Seimbang Pasien')
-    Fatigue = st.number_input('Gejala Kelelahan')
-    Wheezing = st.number_input('Suara Saluran Pernapasan')
+# Input form for user
+input_data = st.text_area('Masukkan data pasien (pisahkan dengan koma)', '1,17,1,3,5,3,4,2,3,3,7,8,6,2,7')
+input_data_list = [float(x.strip()) for x in input_data.split(',')]
 
-with c2:
-    Age = st.number_input('Umur Pasien')
-    Alcohol_use = st.number_input('Alkohol yang di konsumsi')
-    Genetic_Risk = st.number_input('Resiko Genetik')
-    Weight_Loss  = st.number_input('Penurunan Berat Badan')
-    Swallowing_Difficulty = st.number_input('Kesulitan Menelan')
+# Reshape the input data
+data_reshaped = np.asarray(input_data_list).reshape(1, -1)
 
-with c3:
-    Gender = st.number_input('Jenis Kelamin Pasien')
-    Dust_Allergy = st.number_input('Alergi Debu')
-    Smoking = st.number_input('Riwayat Paien Merokok')
-    Coughing_of_Blood = st.number_input('Batuk Berdarah')
-    Shortness_of_Breath = st.number_input('Sesak Nafas')
+# Make prediction
+prediction = model.predict(data_reshaped)[0]
 
-prediksi = ''
-if st.button('Hasil Prediksi'):
-    prediksi = model.predict([[ Age,	Gender,	Air_Pollution, Alcohol_use, Dust_Allergy, OccuPational_Hazards,	Genetic_Risk, 
-                                Balanced_Diet, Smoking, Coughing_of_Blood, Fatigue, 
-                               Weight_Loss, Shortness_of_Breath, Wheezing, Swallowing_Difficulty]])
-
-    if (prediksi [0] == 0):
-        prediksi = 'Keparahan Kanker Paru-Paru Pasien Berada di Tingkat Tinggi'
-    elif(prediksi == 2):
-        prediksi = 'Keparahan Kanker Paru-Paru Pasien Berada di Tingkat Sedang'
-    else:
-        prediksi = 'Keparahan Kanker Paru-Paru Pasien Berada di Tingkat Rendah'
-st.success(prediksi)
+# Display the prediction
+if prediction == 2:
+    st.write('Keparahan Kanker Paru-Paru Pasien Berada di Tingkat Tinggi')
+elif prediction == 1:
+    st.write('Keparahan Kanker Paru-Paru Pasien Berada di Tingkat Sedang')
+elif prediction == 0:
+    st.write('Keparahan Kanker Paru-Paru Pasien Berada di Tingkat Rendah')
+else:
+    st.write('Tingkat keparahan tidak dikenali')
