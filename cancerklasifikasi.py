608d0842af5607853367ae82ea2637f5aa0e3904
Muhammad Irpan <120390988+Smillerby@users.noreply.github.com>
2023-12-24 22:09:06 +0700

    Update cancerklasifikasi.py

--- cancerklasifikasi.py
+++ cancerklasifikasi.py
@@ -4,13 +4,17 @@ import numpy as np
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 
-# ... (rest of your code)
+# ... (bagian lain dari kode Anda)
 
-# Assuming X and Y are your original features and labels
+# Mengasumsikan data awal Anda berada dalam DataFrame yang disebut 'df'
+X = df.drop('Level', axis=1)  # Fitur
+Y = df['Level']  # Label
+
+# Membagi data menjadi set pelatihan dan pengujian
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=2)
 
-# Load the model
-filename = 'cancer patient data sets.sav'
+# Memuat model
+filename = 'cancer-patients-and-air-pollution-a-new-link/cancer patient data sets.sav'
 model1 = pickle.load(open(filename, 'rb'))
 
 # ...
@@ -19,22 +23,22 @@ model1 = pickle.load(open(filename, 'rb'))
 y_true = y_test
 y_pred = model1.predict(x_test)
 
-# Streamlit App
-st.title("Keparahan Kanker Paru-Paru Prediction App")
+# Aplikasi Streamlit
+st.title("Aplikasi Prediksi Keparahan Kanker Paru-Paru")
 
-# Input form for user
+# Formulir input untuk pengguna
 st.sidebar.header("Masukkan Data Pasien:")
 input_data = []
 for i in range(15):
     input_data.append(st.sidebar.number_input(f"Fitur {i+1}", min_value=0, value=0))
 
-# Button to make predictions
+# Tombol untuk membuat prediksi
 if st.sidebar.button("Prediksi"):
     severity_prediction = model1.predict(np.asarray(input_data).reshape(1, -1))[0]
     st.subheader("Hasil Prediksi:")
     st.write(f'Keparahan Kanker Paru-Paru Pasien Berada di Tingkat {severity_prediction}')
 
-# Display evaluation metrics
+# Menampilkan metrik evaluasi
 st.subheader("Confusion Matrix:")
 st.text(confusion_matrix(y_true, y_pred))
 
