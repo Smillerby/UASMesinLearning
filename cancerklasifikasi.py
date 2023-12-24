50fd4fac72bc054fea29bb66726a69b253abba85
Muhammad Irpan <120390988+Smillerby@users.noreply.github.com>
2023-12-24 21:55:50 +0700

    Update cancerklasifikasi.py

--- cancerklasifikasi.py
+++ cancerklasifikasi.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
 
 # Fungsi untuk mengonversi input data menjadi format yang dapat digunakan oleh model
 def preprocess_input(input_data):
@@ -14,7 +15,7 @@ def predict_severity(input_data, model):
     return prediction[0]
 
 # Load model dari file pickle
-filename = 'cancer patient data sets.sav'
+filename = 'cancer-patients-and-air-pollution-a-new-link/cancer patient data sets.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 
 # Judul aplikasi web
@@ -22,13 +23,19 @@ st.title("Aplikasi Keparahan Kanker Paru-Paru")
 
 # Input form untuk pengguna
 st.sidebar.header("Masukkan Data Pasien:")
-input_data = []
-for i in range(15):
-    input_data.append(st.sidebar.number_input(f"Fitur {i+1}", min_value=0, value=0))
+
+# Inisialisasi data frame dengan data pasien
+data_columns = ['Age', 'Air Pollution', 'Alcohol use', 'Dust Allergy', 'OccuPational Hazards', 
+                'Genetic Risk', 'chronic Lung Disease', 'Fatigue', 'Weight Loss', 
+                'Shortness of Breath', 'Wheezing', 'Swallowing Difficulty', 
+                'Clubbing of Finger Nails', 'Frequent Cold', 'Dry Cough', 'Snoring']
+input_data = {}
+for column in data_columns:
+    input_data[column] = st.sidebar.number_input(column, min_value=0, value=0)
 
 # Tombol untuk memprediksi
 if st.sidebar.button("Prediksi"):
-    severity_prediction = predict_severity(input_data, loaded_model)
+    severity_prediction = predict_severity(list(input_data.values()), loaded_model)
     st.subheader("Hasil Prediksi:")
     if severity_prediction == 0:
         st.write('Keparahan Kanker Paru-Paru Pasien Berada di Tingkat Tinggi')
